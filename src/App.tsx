cc605a29018e66b78d6bafed98b1edb1955ff826
Sobo Martin Iulian <35849925+Iulian85@users.noreply.github.com>
2025-12-06 18:37:16 +0100

    MAJOR UPDATE: Full Eliezer Rush style game + wallet connect + airdrop live

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       <Canvas 
         shadows 
         // Moved camera closer (Z: 19.5) for a more immersive, zoomed-in view
-        camera={{ position: [0, 0,19], fov: 40 }}
+        camera={{ position: [0, 0,19], fov: 50 }}
         dpr={[1, 2]} 
         className="touch-none"
       >
